23f99571b365496f58fab294aec3e04d13efcda7
Jessie Gautherot <jessie.gautherot@gmail.com>
2025-01-18 16:53:46 +0100

    add NotFoundPage

--- Portfolio Jessie Gautherot/src/App.vue
+++ Portfolio Jessie Gautherot/src/App.vue
@@ -8,10 +8,7 @@
 
     
 
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
+      
    
   </header>
 
--- Portfolio Jessie Gautherot/src/views/AboutView.vue
+++
@@ -1,9 +0,0 @@
-<template>
- 
-    <h1>ca va etre l'erreur 404</h1>
-  
-</template>
-
-<style>
-
-</style>
