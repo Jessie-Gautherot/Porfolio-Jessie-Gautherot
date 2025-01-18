3c5015d3b46a4793c7551481509bc254185691c4
Jessie Gautherot <jessie.gautherot@gmail.com>
2025-01-18 16:30:15 +0100

    add HomeWiew

--- Portfolio Jessie Gautherot/src/views/HomeView.vue
+++ Portfolio Jessie Gautherot/src/views/HomeView.vue
@@ -4,6 +4,42 @@
 
 <template>
   <main>
-    
+    <section id="about">
+      <h1>A propos de moi</h1>
+        <p>kjlkjlkjkljlkjkljkljklj
+       Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint eveniet atque, dolorum mollitia consequuntur corporis ipsa ea porro, molestias ex magni, reiciendis quaerat laboriosam delectus labore. Voluptatum quia velit quaerat.</p>
+    </section>
+    <section id="works">
+      <h1>Mes réalisations</h1>
+        <div>
+          <h2>réalisation 1</h2>
+            <p>jkjkjk</p>
+        </div>
+        <div>
+          <h2>réalisation 2</h2>
+            <p>nkljkljl</p>
+        </div>
+        <div>
+          <h2>réalisation 3</h2>
+            <p>hhkjhkhk</p>
+        </div>
+    </section>
+    <section id="contact">
+      <h1>Contactez-moi</h1>
+        <form></form>
+    </section>
   </main>
 </template>
+
+<style scoped>
+
+main {
+  background-color: brown;
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  margin: 0;
+}
+
+
+</style>
