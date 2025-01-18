4b28737485b95da075f9ec3f7d0e05e2bd219280
Jessie Gautherot <jessie.gautherot@gmail.com>
2025-01-18 17:45:40 +0100

    add TheFooter

---
+++ Portfolio Jessie Gautherot/src/components/TheFooter.vue
@@ -0,0 +1,51 @@
+<script setup>
+
+</script>
+
+<template>
+  <footer>
+    <div class="logo">
+      <img src="../assets/facebook(1).png" alt="logo facebook">
+      <a href="https://www.facebook.com/jessie.gautherot/" target="_blank" rel="noopener noreferrer"></a>
+      <img src="../assets/github.png" alt="logo github">
+      <a href="https://github.com/Jessie-Gautherot" target="_blank" rel="noopener noreferrer"></a>
+      <img src="../assets/instagram(1).png" alt="logo instagram">
+      <a href="https://www.instagram.com/epicalys" target="_blank" rel="noopener noreferrer"></a>
+    </div>
+    <div>
+      <small>
+       <p>Dernière modification le <time datetime="2025-01-18">18 janvier 2025</time> </p>
+      </small>
+    </div>
+  </footer>
+</template>
+
+<style scoped>
+
+footer {
+  background-color:  rgb(44, 44, 38);
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: auto;
+  width: 100%;
+  box-sizing: border-box;
+  margin: 0%;
+  padding: 0%;
+  border: 0%;
+}
+
+.logo {
+  display: flex;
+  flex-direction: row;
+}
+
+img {
+  width: 60px;
+  height: 60px;
+  padding: 10px;
+  margin-bottom: 20px;
+}
+
+</style>
