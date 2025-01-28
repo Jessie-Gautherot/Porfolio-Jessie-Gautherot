fe28384fbe993f951ed9878f3f98c017ff3a9eec
Jessie Gautherot <jessie.gautherot@gmail.com>
2025-01-28 11:47:02 +0100

    add AboutMe

---
+++ Portfolio Jessie Gautherot/src/components/AboutMe.vue
@@ -0,0 +1,41 @@
+<template>
+  <main>
+    <div>
+        <h1>A propos de moi</h1>
+        <p>Bonjour, je m'appelle Jessie Gautherot et je suis passionnée par le développement web.
+Actuellement, je suis en formation de développeur web et web mobile au CEF. J'ai eu l'opportunité de travailler 
+sur plusieurs projets, qui m'ont permis d'acquérir une solide base dans le développement d'interfaces utilisateur 
+et de développer mes compétences techniques, notamment avec des frameworks modernes comme Vue.js.</p>
+<br>
+<p>Ce qui me plaît dans le développement web, c'est la possibilité de donner vie à des idées.
+J'aime créer des interfaces intuitives et interactives qui répondent aux besoins des utilisateurs. 
+Cela me permet de combiner ma créativité avec des compétences techniques, pour proposer des solutions efficaces et agréables.</p>
+<br>
+<p>Mon objectif est de continuer à évoluer, pour contribuer à des projets plus complexes,
+tout en perfectionnant mes compétences techniques et créatives.</p>
+</div>
+</main>
+
+</template>
+
+<script setup>
+
+
+</script>
+
+<style scoped>
+div {
+  
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 550px;
+}
+
+h1 {
+  font-size: xx-large;
+  color: #156062;
+}
+
+</style>
--- Portfolio Jessie Gautherot/src/views/HomeView.vue
+++ Portfolio Jessie Gautherot/src/views/HomeView.vue
@@ -1,23 +1,12 @@
 <script setup>
 import ContactForm3 from '@/components/ContactForm3.vue';
+import AboutMe from '@/components/AboutMe.vue';
 </script>
 
 <template>
   <main>
-    <section id="about" class="section">
-        <h1>A propos de moi</h1>
-        <p>Bonjour, je m'appelle Jessie Gautherot et je suis passionnée par le développement web.
-Actuellement, je suis en formation de développeur web et web mobile au CEF. J'ai eu l'opportunité de travailler 
-sur plusieurs projets, qui m'ont permis d'acquérir une solide base dans le développement d'interfaces utilisateur 
-et de développer mes compétences techniques, notamment avec des frameworks modernes comme Vue.js.</p>
-<br>
-<p>Ce qui me plaît dans le développement web, c'est la possibilité de donner vie à des idées.
-J'aime créer des interfaces intuitives et interactives qui répondent aux besoins des utilisateurs. 
-Cela me permet de combiner ma créativité avec des compétences techniques, pour proposer des solutions efficaces et agréables.</p>
-<br>
-<p>Mon objectif est de continuer à évoluer, pour contribuer à des projets plus complexes,
-tout en perfectionnant mes compétences techniques et créatives.</p>
-    </section>
+    <section id="about" class="section"></section>
+    <AboutMe></AboutMe>
 
     <section id="works" class="section">
       <h1>Mes réalisations</h1>
@@ -63,14 +52,6 @@ section {
   align-items: center;
 }
 
-#about {
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  width: 550px;
-}
-
 
 #works div {
   display: flex;
@@ -79,17 +60,6 @@ section {
   align-items: center;
   padding-bottom: 50px;
 }
-
-
-
-/*#contact {
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-
-}*/
-
 
 img {
   width: 200px;
